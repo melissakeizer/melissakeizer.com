82f18c7c0be18f596040a88e97b47f09c5698242
Melissa Keizer <mkeizer@rumblelabs.com>
2016-10-19 00:04:36 +0100

    adding portfolio images and newly saved out images, responsive styles, mobile menu

--- js/all.js
+++ js/all.js
@@ -1,22 +1,4 @@
 $(document).ready(function() {
-
-
-  // Matching the grid square's height with thier fluid width
-   var itemwidth = $('footer .photos').height();
-  $('.instagram-block').css({
-    'height': itemwidth + 'px'
-  });
-
-
-
-  // Randomising the background image set for the flickr div
-  var images = ['squirrel-mkeizer.jpg', 'sumatran-tiger-mkeizer.jpg', 'giants-causeway-mkeizer.jpg', 'matisse-mkeizer.jpg', 'cleo-mkeizer.jpg', 'ladygaga-mkeizer.jpg', 'glenoe-mkeizer.jpg', 'lighthouse-mkeizer.jpg' ];
-  $('ul.home-grid li.flickr').css({'background-image': 'url(img/photography-array/' + images[Math.floor(Math.random() * images.length)] + ')'});
-
-  // Randomising the background image set for the flickr div
-  var shots = ['belfast-ruby-macbook.jpg', 'reverspective.jpg', 'the-laboratory.jpg', 'belfast-ruby.jpg', 'SRLabs.jpg'];
-  $('ul.home-grid li.dribbble').css({'background-image': 'url(img/dribbble-shots/' + shots[Math.floor(Math.random() * shots.length)] + ')'});
-
   // Mobile navigation
   $('#hamburger').on('click', function(){
     $('#mobile-menu').css("display", "block");
