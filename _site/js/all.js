971b7ddb77f75bc1ed92ee24ae380e5c976bf558
Melissa Keizer <mkeizer@rumblelabs.com>
2016-10-22 18:48:27 +0100

    bunch of updates i cant remember

--- _site/js/all.js
+++ _site/js/all.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  $('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
   // Mobile navigation
-  $('#hamburger').on('click', function(){
+  $('#close-menu').on('click', function( event ){
+    event.preventDefault();
+    $('#mobile-menu').css("display", "none");
+  });
+  $('#hamburger').on('click', function( event ){
+    event.preventDefault();
     $('#mobile-menu').css("display", "block");
   });
-  $('#close-menu').on('click', function( event ){
-      event.preventDefault();
-      $('#mobile-menu').css("display", "none");
-  });
-
-
 });
